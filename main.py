c62bbe2a437cb76b8ad626cb6809b9e5cc226c32
wiktor.kobiela <wuch.mail@gmail.com>
2021-03-15 13:19:37 +0100

    Working automated script to check all new commits

--- main.py
+++ main.py
@@ -4,50 +4,55 @@ import time
 
 
 def pull():
-    os.popen('cd ../Dockerfiles/; git pull') #change your local repo directory
+    os.popen('cd ../Dockerfiles/; git pull')  # change your local repo directory
     time.sleep(2)
     return
 
-
 def job():
     file = open("commit.txt", "r+")
-    previous_checked = file.read()
-    print("Previously: " + previous_checked)
-
-    output = os.popen('cd ../Dockerfiles/; git log -1 --pretty=%B').read() #change your local repo directory
-    output_oneline = os.linesep.join([s for s in output.splitlines() if s])
-
-    print("New check: " + output_oneline)
+    previous_checked1 = file.read()
+    previous_checked = os.linesep.join([s for s in previous_checked1.splitlines() if s])
 
     time.sleep(2)
+    same = 0
+    count = 0
 
-    if previous_checked != output_oneline:
-        print("New commit!")
-        apo = '"'
-        command = "./discord.sh " + "--username OpenVisualCloud " + "--avatar " + apo \
-                  + "https://avatars3.githubusercontent.com/u/46843401?s=90&v=4" + apo \
-                  + " --text " + apo + "🐳 NEW COMMIT: " + "**" + output_oneline + "**" \
-                  + " -path: <https://github.com/OpenVisualCloud/Dockerfiles/commits/v2.0>" + apo
-        # + " --title " + apo + "Check more here" + apo\
-        # + " --url " + apo + "https://github.com/OpenVisualCloud/Dockerfiles/commits/v2.0" + apo
-        os.popen(command)
-        print(command)
+    while same != 1:
+        output = os.popen('cd ../Dockerfiles/; git log -1 --skip ' + str(count) + ' --pretty=format:%s').read()  # change your local repo directory
+        #print('Odczytany commit: ' + output)
+        # output_oneline = os.linesep.join([s for s in output.splitlines() if s])
 
-        file.seek(0)
-        file.truncate()
-        file.write(output_oneline)
-        file.close()
+        print("From file: " + previous_checked)
+        print("New check: " + output)
 
-        time.sleep(2)
-        # quit("Znalazłem update i spadam.")
-        print("-----------------------------------------------")
+        if previous_checked == output:
+            print("No new updates.")
+            latest_commit = os.popen('cd ../Dockerfiles/; git log -1 --pretty=format:%s').read()
+            file.seek(0)
+            file.truncate()
+            file.write(latest_commit)
+            time.sleep(2)
+            # quit("Nic sie nie dzieje, wiec spadam.")
+            print("-----------------------------------------------")
+            file.close()
+            same = 1
+            quit("All done")
 
-    else:
-        print("No new updates.")
-        file.close()
-        time.sleep(2)
-        # quit("Nic sie nie dzieje, wiec spadam.")
-        print("-----------------------------------------------")
+        else:
+            print("New commit!")
+            apo = '"'
+            command = "./discord.sh " + "--username OpenVisualCloud " + "--avatar " + apo \
+                      + "https://avatars3.githubusercontent.com/u/46843401?s=90&v=4" + apo \
+                      + " --text " + apo + "🐳 NEW COMMIT: " + "**" + output + "**" \
+                      + " -path: <https://github.com/OpenVisualCloud/Dockerfiles/commits/v2.0>" + apo
+            # + " --title " + apo + "Check more here" + apo\
+            # + " --url " + apo + "https://github.com/OpenVisualCloud/Dockerfiles/commits/v2.0" + apo
+            os.popen(command)
+            print(command)
+            count = count + 1
+            print(count)
+            time.sleep(2)
+            print("-----------------------------------------------")
 
     return
 
