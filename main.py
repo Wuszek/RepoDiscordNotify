ba16f4c7f5638d139e63f13c017549fc53cea083
wiktor.kobiela <wuch.mail@gmail.com>
2021-03-16 14:20:37 +0100

    Added logging to file and program runs selected number of loops

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # !/usr/bin/python
 import os
 import time
+import sys
 from datetime import datetime
 
 repo = "../Test2/"  # repository path relative to script location
+counter = 0
 
 
 def pull():
     os.popen('cd ' + repo + '; git pull')  # change your local repo directory
-    time.sleep(2)
+    time.sleep(1)
     return
 
 
@@ -17,35 +19,31 @@ def job():
     previous_checked1 = file.read()
     previous_checked = os.linesep.join([s for s in previous_checked1.splitlines() if s])
 
-    time.sleep(2)
-    # same = 0
+    time.sleep(1)
     count = 0
 
     while True:
         output = os.popen('cd ' + repo + '; git log -1 --skip ' + str(count) + ' --pretty=format:%s').read()
         commit_hash = os.popen('cd ' + repo + '; git log -1 --skip ' + str(count) + ' --pretty=format:%H').read()
-        # commit_date = os.popen('cd ../Dockerfiles/; git log -1 --skip ' + str(count) + ' --pretty="format:%ar"').read()
-
-        now = datetime.now()  # current date and time
-        date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+        # commit_date = os.popen('cd ' + repo + '; git log -1 --skip ' + str(count) + ' --pretty="format:%ar"').read()
 
         # print(commit_hash)
         print("Last commit: " + previous_checked)
         print("Now checked: " + output)
 
         if previous_checked == output:
-            print("No new updates. Finished at " + date_time)
+            print("No new updates.")
             latest_commit = os.popen('cd ' + repo + '; git log -1 --pretty=format:%s').read()
             file.seek(0)
             file.truncate()
             file.write(latest_commit)
-            time.sleep(2)
+            time.sleep(1)
             print("-----------------------------------------------")
             file.close()
-            # same = 1
             # quit("All done")
-            print("Sleeping for 30s now \n")
-            time.sleep(30)
+            print("Sleeping for 1min now \n")
+            print("-----------------------------------------------")
+            time.sleep(60)  # set break after no new commits to 60s
             break
 
         else:
@@ -63,10 +61,16 @@ def job():
             time.sleep(2)
             print("-----------------------------------------------")
 
-    # return
 
+# if __name__ == '__main__':  # change main with while, to run script in loop
+# while True:
+while counter < 10:  # set counter
+    now = datetime.now()
+    loop_time = now.strftime("%d/%m/%Y, %H:%M:%S")
 
-# if __name__ == '__main__':
-while True:
+    sys.stdout = open('log.txt', 'a+')
+    print("Next loop started: " + loop_time)
     pull()
     job()
+    sys.stdout.close()
+    counter += 1
