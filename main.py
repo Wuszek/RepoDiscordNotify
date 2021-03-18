9273b61f33bc714f47b6008d3865c99df46b8278
wiktor.kobiela <wuch.mail@gmail.com>
2021-03-18 14:41:48 +0100

    Check if provided url is valid .git repo url. Set default branch as master

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def clone(repo, branch, dir_name):
             print("Repository already cloned with selected branch.")
         else:
             print("Repository cloned, but with wrong branch. Removing old, cloning proper one.")
-            os.popen('cd ../; rm -rf ' + dir_name + '; git clone --single-branch --branch ' + branch + " " + repo).read()
+            os.popen('cd ../; rm -rf ' + dir_name + '; git clone \
+                     --single-branch --branch ' + branch + " " + repo).read()
     else:
         os.popen('cd ../; git clone --single-branch --branch ' + branch + ' ' + repo).read()
 
@@ -79,17 +80,20 @@ def job(dir_name):
 
 
 def usage():
-    print("python3 main.py --repo <link_to_repo> --branch <branch_to_be_observed>")
+    print("====================================== DISPLAYING HELP ======================================")
+    print("python3 main.py --repo <link_to_repository> --branch <branch_to_be_observed (default master)>")
     print("--repo: Link to clone repo, with .git at the end")
     print("--branch: Branch name, that will be cloned")
     print("e.g.")
     print("python3 main.py --repo https://github.com/OpenVisualCloud/Dockerfiles.git --branch v21.3")
+    print("=============================================================================================\n")
     sys.exit()
 
 
 def getarguments(argv):
+    global opts
     repo = None
-    branch = None
+    branch = "master"
     short_opts = 'hr:b:'
     long_opts = ["help", "repo=", "branch="]
     try:
@@ -98,7 +102,7 @@ def getarguments(argv):
             print("Error: No options supplied")
             usage()
     except getopt.GetoptError:
-        print(f"Error in options {opts}")
+        print(f"Error in options or options not specified.\n")
         usage()
 
     for opt, arg in opts:
@@ -106,6 +110,12 @@ def getarguments(argv):
             repo = arg
         elif opt in ("-b", "--branch"):
             branch = arg
+            if re.match(r"/^(([A-Za-z0-9]+@|http(|s)\:\/\/)|(http(|s)\:\/\/[A-Za-z0-9]+@))([A-Za-z0-9.]\
+                        +(:\d+)?)(?::|\/)([\d\/\w.-]+?)(\.git){1}$/i", branch):
+                print("Valid git repository url. Continuing...")
+            else:
+                print("Invalid git repository url.\n")
+                usage()
         elif opt in ("-h", "--help"):
             usage()
         else:
@@ -116,16 +126,16 @@ def getarguments(argv):
 
 if __name__ == '__main__':
     repo, branch = getarguments(sys.argv[1:])
-    dir_name = re.search(r"(([^\/]+).{4})$", repo).group(2)
+    dir_name = re.search(r"(([^/]+).{4})$", repo).group(2)
 
     now = datetime.now()
     loop_time = now.strftime("%d/%m/%Y, %H:%M:%S")
     # sys.stdout = open('log.txt', 'a+')
     print("Program started: " + loop_time)
     clone(repo, branch, dir_name)
-    while True: # or while counter < given number of loops
+    while True:  # or while counter < given number of loops
         pull(dir_name)
         job(dir_name)
         # counter += 1
     # sys.stdout.close()
-    exit()
+    # exit()
