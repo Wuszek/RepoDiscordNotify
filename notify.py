4713f4d7e3e586cb8b9c3a4524be54467c1aa77a
wiktor.kobiela <wuch.mail@gmail.com>
2021-03-19 17:25:07 +0100

    Added check for only positive int value

--- notify.py
+++ notify.py
@@ -100,12 +100,18 @@ def argument_parse(argv):
         print("Valid git repository url. Proceeding...")
         return arg_value
 
+    def positive_int(value):
+        ivalue = int(value)
+        if ivalue <= 0:
+            raise argparse.ArgumentTypeError("%s is an invalid positive int value." % value)
+        return ivalue
+
     parser.add_argument('-r', '--repo', action='store', dest="repo", help="repository link to cloned repo,\
                         with .git at the end", type=git_repo_regex, required=True)
     parser.add_argument('-b', '--branch', action='store', dest="branch", help="branch name, that will be cloned - \
                         default is master", default="master")
     parser.add_argument('-t', '--time', action='store', dest="time", help="idle time between next pull&check - default \
-                        is 10s", type=int, default=10)
+                        is 10s", type=positive_int, default=10)
     parser.add_argument('-v', '--version', action='version', version='%(prog)s alpha')
 
     args = parser.parse_args()
@@ -116,14 +122,14 @@ if __name__ == '__main__':
     loop_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     # sys.stdout = open('log.txt', 'a+')  # Comment this, to enable live logging in terminal
     repo, branch, sleep_time = argument_parse(sys.argv[1:])
-    print(f'Setup idle time {sleep_time}')
+    print(f'Setup idle time: {sleep_time}')
     print("Program started: " + loop_time)
     dir_name = re.search(r"(([^/]+).{4})$", repo).group(2)
-    get_files()
-    clone(repo, branch, dir_name)
-    while True:  # Or while counter < given_number, to get finite number of loops
-        pull(dir_name)
-        job(dir_name)
+    # get_files()
+    # clone(repo, branch, dir_name)
+    # while True:  # Or while counter < given_number, to get finite number of loops
+    #     pull(dir_name)
+    #     job(dir_name)
       # counter += 1
     # sys.stdout.close()  # Comment this, to enable live logging in terminal
     # exit()  # Uncomment, if using finite number of loops
