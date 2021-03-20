67a6fd09b9d99ab8fa29b6863128710a0df35768
wiktor.kobiela <wuch.mail@gmail.com>
2021-03-20 14:21:55 +0100

    Check if .webhook file exists

--- notify.py
+++ notify.py
@@ -21,6 +21,11 @@ def get_files():
         open(filename, 'wb').write(f.content)
         os.popen('chmod +x discord.sh').read()
         print("File 'discord.sh' downloaded. Proceeding...")
+
+    if os.path.isfile('.webhook'):
+        print(".webhook file fund!")
+    else:
+        exit("No .webhook file. Create one with webhook url inside.")
     return
 
 
